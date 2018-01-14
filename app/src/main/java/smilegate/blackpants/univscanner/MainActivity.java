c4554566190857e53e940fe01373ba32dd95a095
Semin <cing9393@gmail.com>
2018-01-15 00:51:18 +0900

    버터나이프 적용

--- app/src/main/java/smilegate/blackpants/univscanner/MainActivity.java
+++ app/src/main/java/smilegate/blackpants/univscanner/MainActivity.java
@@ -1,14 +1,26 @@
 package smilegate.blackpants.univscanner;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
+    @BindView(R.id.btn_fb_login)
+    Button fbLoginBtn;
+    @OnClick(R.id.btn_fb_login)
+    public void facebookLogin(Button button) {
+        Toast.makeText(this, "complete!", Toast.LENGTH_SHORT).show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
