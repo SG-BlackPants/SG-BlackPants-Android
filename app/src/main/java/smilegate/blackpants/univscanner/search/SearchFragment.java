e3b3b7ac2c876e1f8a2cb53b10bc4c00a8e69e85
Semin <cing93@naver.com>
2018-01-22 18:09:58 +0900

    검색 앱바 클릭 작성

--- app/src/main/java/smilegate/blackpants/univscanner/search/SearchFragment.java
+++ app/src/main/java/smilegate/blackpants/univscanner/search/SearchFragment.java
@@ -5,7 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import smilegate.blackpants.univscanner.R;
 
 /**
@@ -15,6 +20,19 @@ import smilegate.blackpants.univscanner.R;
 public class SearchFragment extends Fragment{
     private static final String TAG = "SearchFragment";
     private static final int ACTIVITY_NUM = 0;
+    private View view;
+
+    @BindView(R.id.layout_search_toolbar)
+    View includeLayoutSearch;
+
+    @BindView(R.id.reLayout_search)
+    RelativeLayout relLayoutSearch;
+
+    @OnClick(R.id.layout_search_toolbar)
+    public void search(View appBarLayout) {
+        Toast.makeText(getActivity(), "검색클릭!",
+                Toast.LENGTH_SHORT).show();
+    }
 
     public static SearchFragment newInstance() {
         SearchFragment fragment = new SearchFragment();
@@ -29,8 +47,16 @@ public class SearchFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_search, null);
+        ButterKnife.bind(this, view);
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
 
 
 }
