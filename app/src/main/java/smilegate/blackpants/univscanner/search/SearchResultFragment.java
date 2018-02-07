96ccbcbeec95267c1d8bd32675027648129d4d8e
Semin <cing9393@gmail.com>
2018-02-07 23:55:08 +0900

    검색 결과 화면 drawer 추가

--- app/src/main/java/smilegate/blackpants/univscanner/search/SearchResultFragment.java
+++ app/src/main/java/smilegate/blackpants/univscanner/search/SearchResultFragment.java
@@ -2,12 +2,14 @@ package smilegate.blackpants.univscanner.search;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import java.util.ArrayList;
@@ -37,6 +39,7 @@ public class SearchResultFragment extends BaseFragment {
 
     @BindView(R.id.btn_searchresult_back)
     ImageButton searchResultBackBtn;
+
     @OnClick(R.id.btn_searchresult_back)
     public void searchResultBack(ImageButton imageButton) {
         if (!mNavController.popFragment()) {
@@ -46,6 +49,23 @@ public class SearchResultFragment extends BaseFragment {
 
     @BindView(R.id.list_searchresult)
     RecyclerView searchResultRecyclerView;
+
+    @BindView(R.id.drawerLayout_searchresult)
+    DrawerLayout drawerLayout;
+
+    /*@BindView(R.id.drawer_filter)
+    View drawerView;
+*/
+    @BindView(R.id.nvView)
+    View drawerView;
+
+    @BindView(R.id.btn_filter)
+    Button filterBtn;
+
+    @OnClick(R.id.btn_filter)
+    public void filterClick(Button button) {
+        drawerLayout.openDrawer(drawerView);
+    }
 
     public static SearchResultFragment newInstance(int instance) {
         Bundle args = new Bundle();
@@ -98,5 +118,34 @@ public class SearchResultFragment extends BaseFragment {
         mSearchResultsList.add(8, searchResults);
     }
 
+    DrawerLayout.DrawerListener myDrawerListener = new DrawerLayout.DrawerListener() {
+
+        public void onDrawerClosed(View drawerView) {
+        }
+        public void onDrawerOpened(View drawerView) {
+        }
+
+        public void onDrawerSlide(View drawerView, float slideOffset) {
+          /*  txtPrompt.setText("onDrawerSlide: "
+                    + String.format("%.2f", slideOffset));*/
+        }
+
+        public void onDrawerStateChanged(int newState) {
+            String state;
+            switch (newState) {
+                case DrawerLayout.STATE_IDLE:
+                    state = "STATE_IDLE";
+                    break;
+                case DrawerLayout.STATE_DRAGGING:
+                    state = "STATE_DRAGGING";
+                    break;
+                case DrawerLayout.STATE_SETTLING:
+                    state = "STATE_SETTLING";
+                    break;
+                default:
+                    state = "unknown!";
+            }
+        }
+    };
 
 }
